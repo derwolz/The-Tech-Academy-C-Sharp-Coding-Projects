73c5aba7df1a0fcaae38ea6b4c97a2423b9a7e3f
Blaine Wolz <der.wolz@gmail.com>
2020-01-12 22:10:14 -0700

    add primary key in database for CustomerCar add Quote calculation method

--- CarQuote/CarQuote/Controllers/AdminController.cs
+++ CarQuote/CarQuote/Controllers/AdminController.cs
@@ -17,35 +17,38 @@ namespace CarQuote.Controllers
             {
                 
                 List<CustomerVM> Customers = new List<CustomerVM>();
-                var signups = (from c in db.Customers join d in db.CustomerCars on c.Id equals d.CustomerId 
-                               join e in db.CustomerRecords on c.Id equals e.CustomerId select  new
-                               {
-                                   c.FirstName,
-                                   c.LastName,
-                                   c.EmailAddress,
-                                   c.DateofBirth,
-                                   d.CarYear,
-                                   d.Make,
-                                   d.Model,
-                                   e.DUI,
-                                   e.FullCoverage,
-                                   e.NumofTicket
-                               });
+
                 
-                CustomerVM Customer = new CustomerVM();
-                foreach (var person in signups)
+                foreach (var person in (from c in db.Customers
+                                        join d in db.CustomerCars on c.Id equals d.CustomerId
+                                        join e in db.CustomerRecords on c.Id equals e.CustomerId
+                                        select new
+                                        {
+                                            c.FirstName,
+                                            c.LastName,
+                                            c.EmailAddress,
+                                            c.DateofBirth,
+                                            d.CarYear,
+                                            d.Make,
+                                            d.Model,
+                                            e.DUI,
+                                            e.FullCoverage,
+                                            e.NumofTicket
+                                        }).ToList())
                 {
-                    
-                    Customer.Birthday = (DateTime)person.DateofBirth;
-                    Customer.FirstName = person.FirstName;
-                    Customer.LastName = person.LastName;
-                    Customer.EmailAddress = person.EmailAddress;
-                    Customer.CarMake = person.Make;
-                    Customer.CarModel = person.Model;
-                    Customer.CarYear = person.CarYear;
-                    Customer.DUI = person.DUI;
-                    Customer.FullCoverage = person.FullCoverage;
-                    Customer.NumofTicket = person.NumofTicket;
+                    CustomerVM Customer = new CustomerVM
+                    {
+                        Birthday = (DateTime)person.DateofBirth,
+                        FirstName = person.FirstName,
+                        LastName = person.LastName,
+                        EmailAddress = person.EmailAddress,
+                        CarMake = person.Make,
+                        CarModel = person.Model,
+                        CarYear = person.CarYear,
+                        DUI = person.DUI,
+                        FullCoverage = person.FullCoverage,
+                        NumofTicket = person.NumofTicket
+                    };
                     Customers.Add(Customer);
                 }
 
--- CarQuote/CarQuote/ViewModel/CustomerVM.cs
+++ CarQuote/CarQuote/ViewModel/CustomerVM.cs
@@ -19,12 +19,19 @@ namespace CarQuote.ViewModel
         public bool DUI { get; set; }
         public int NumofTicket { get; set; }
 
-        public int GetQuote()
+        public double GetQuote()
         {
-            int Quote = 50;
+            double Quote = 50f;
             if (Birthday > DateTime.Now.AddYears(-25)) Quote += 25;
             if (Birthday > DateTime.Now.AddYears(-18)) Quote += 100;
             if (Birthday < DateTime.Now.AddYears(-100)) Quote += 100;
+            if (CarYear < 200) Quote += 25;
+            if (CarMake.ToLower() == "porsche") Quote += 25;
+            if (CarMake.ToLower() == "porsche" && CarModel == "911 Carrera") Quote += 25;
+            for (int i = 0; i < NumofTicket; i++) Quote += 10;
+            if (DUI == true) Quote *= 1.25;
+            if (FullCoverage == true) Quote *= 1.5;
+            return Quote;
         }
     }
 }
